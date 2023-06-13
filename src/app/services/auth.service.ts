5405fc43d6bdb3d1735f31e04df8a921a7f18718
Juan Rodriguez <aire19527@gmail.com>
2023-06-13 09:10:17 -0500

    ajuste

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,75 +1,81 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { UserModel } from '../models/user.model';
-import { map } from 'rxjs/operators';
-import { CompanyModel } from '../models/company.model';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { UserModel } from "../models/user.model";
+import { map } from "rxjs/operators";
+import { CompanyModel } from "../models/company.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
-
-  private url = 'https://apitechnique20210622070657.azurewebsites.net/api/';
-  //private url = ' http://localhost:8081/api/';
+  //private url = 'https://apitechnique20210622070657.azurewebsites.net/api/';
+  private url = " http://localhost:5001/api/";
 
   userToken: string;
-  //login 
+  //login
   //   http://localhost:8081/api/Login/Login
-  
 
   constructor(private http: HttpClient) {
     this.readToken();
   }
 
   logout() {
-    if (localStorage.getItem('token')) {
-      localStorage.removeItem('token');
-      localStorage.removeItem('expiresIn');
+    if (localStorage.getItem("token")) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("expiresIn");
     }
   }
 
   login(user: UserModel) {
-    return this.http.post(`${this.url}Login/Login`, user)
+    return this.http.post(`${this.url}Login/Login`, user).pipe(
+      map((resp) => {
+        this.saveToeken(resp["result"].token, resp["result"].expiration);
+        return resp;
+      })
+    );
+  }
+
+  searchCompany(idEmpresa: number) {
+    this.userToken = `Bearer ${this.readToken()}`;
+    const headers = new HttpHeaders({ Authorization: this.userToken });
+
+    return this.http
+      .get(
+        `${
+          this.url
+        }Company/GetCompany?identificacionNumber=${idEmpresa.toString()}`,
+        { headers: headers }
+      )
       .pipe(
-        map(resp => {
-          this.saveToeken(resp["result"].token, resp["result"].expiration);
-          return resp;
+        map((resp) => {
+          return resp["result"];
         })
       );
   }
 
-  searchCompany(idEmpresa: number) {
-    this.userToken =`Bearer ${this.readToken()}`;  
-    const headers = new HttpHeaders({'Authorization': this.userToken});
-    
-    return this.http.get(`${this.url}Company/GetCompany?identificacionNumber=${idEmpresa.toString()}`,{headers:headers})
-    .pipe(
-      map(resp => {
-        return resp["result"];
-      })
-    );
-  }
+  getAllTypeIdentification() {
+    this.userToken = `Bearer ${this.readToken()}`;
+    const headers = new HttpHeaders({ Authorization: this.userToken });
 
-  getAllTypeIdentification()
-  {
-    this.userToken =`Bearer ${this.readToken()}`;  
-    const headers = new HttpHeaders({'Authorization': this.userToken});
-    
-    return this.http.get(`${this.url}/Transversal/GetAllTypeIdentification`,{headers:headers})
-    .pipe(
-      map(resp => {
-        console.log(resp);
-        return resp["result"];
+    return this.http
+      .get(`${this.url}/Transversal/GetAllTypeIdentification`, {
+        headers: headers,
       })
-    );
+      .pipe(
+        map((resp) => {
+          console.log(resp);
+          return resp["result"];
+        })
+      );
   }
   updateCompany(company: CompanyModel) {
-    this.userToken =`Bearer ${this.readToken()}`;  
-    const headers = new HttpHeaders({'Authorization': this.userToken});
+    this.userToken = `Bearer ${this.readToken()}`;
+    const headers = new HttpHeaders({ Authorization: this.userToken });
 
-    return this.http.post(`${this.url}Company/UpdateCompany`, company,{headers:headers})
+    return this.http
+      .post(`${this.url}Company/UpdateCompany`, company, { headers: headers })
       .pipe(
-        map(resp => {
+        map((resp) => {
           console.log(resp);
           return resp;
         })
@@ -78,18 +84,18 @@ export class AuthService {
 
   private saveToeken(idToken: string, expiresIn: number) {
     this.userToken = idToken;
-    localStorage.setItem('token', idToken);
+    localStorage.setItem("token", idToken);
 
     let today = new Date();
     today.setSeconds(expiresIn);
-    localStorage.setItem('expiresIn', today.getTime().toString());
+    localStorage.setItem("expiresIn", today.getTime().toString());
   }
 
   readToken() {
-    if (localStorage.getItem('token')) {
-      this.userToken = localStorage.getItem('token');
+    if (localStorage.getItem("token")) {
+      this.userToken = localStorage.getItem("token");
     } else {
-      this.userToken = '';
+      this.userToken = "";
     }
     return this.userToken;
   }
@@ -99,7 +105,7 @@ export class AuthService {
     if (this.userToken.length < 2) {
       response = false;
     } else {
-      const expiraIn = Number(localStorage.getItem('expiresIn'));
+      const expiraIn = Number(localStorage.getItem("expiresIn"));
       const todayExpira = new Date();
 
       todayExpira.setTime(expiraIn);
